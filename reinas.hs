7b958ddbb43afd9128799431366055ddf5563ab7
guillegard <guillegard95@gmail.com>
2018-12-03 21:08:59 -0600

    almost finish, just adding coments to all the programs

--- reinas.hs
+++ reinas.hs
@@ -5,16 +5,15 @@ import Data.List
 
 reinas :: Int -> [[Int]]
 reinas n = map fst $ foldM siguienteReina ([],[1..n]) [1..n]  where
-
   siguienteReina (y,d) _ = [(x:y, delete x d) | x <- d, verifica x]  where
-
--- Verifica que no hay reinas
-    verifica x = and [x /= c + n && x /= c - n | (n,c) <- zip [1..] y]
+    -- Verifica que no hay reinas
+    verifica reina = and [reina /= c + n && reina /= c - n | (n,c) <- zip [1..] y]
 
 -- Imprime el tablero resuelto
+imprimirTableros y = do
      let n = length y
-     mapM_ (\x -> putStrLn [if z == x then 'Q' else '-' | z <- [1..n]]) y
+     mapM_ (\x -> putStrLn [if z == x then 'R' else '#' | z <- [1..n]]) y
      putStrLn ""
 
 -- Imprime las soluciones para n reinas
-main n = mapM_ printSolution $ reinas n
+main n = mapM_ imprimirTableros $ reinas n
--- sudoku.hs
+++ sudoku.hs
@@ -1,84 +1,102 @@
 import Data.Array
 
-main = do
-    let solución = resuelve tableroSudoku
-    printTablero solución
+-- Definicion de los tipos para un tablero
 
-type Marcado = Int
+-- Numero en una posicion
+type Numero = Int
 
+-- Posicion es una tupla de eneros
 type Posicion = (Int, Int)
 
-type Tablero = Array Posicion Marcado
+--Tablero es un arreglo de posiciones y numeros.
+type Tablero = Array Posicion Numero
 
+--Variable que representa un tablero de sudoku
+--Lo genera a partir del ejemplo de tablero
 tableroSudoku :: Tablero
-tableroSudoku = array ((0, 0), (8, 8)) $ verTablero ejemplo
+tableroSudoku = array ((0, 0), (8, 8)) $ creaTablero ejemplo
 
 -- Ejemplo de como escribir el sudoku
-ejemplo :: [[Marcado]]
+ejemplo :: [[Numero]]
 ejemplo = [[5, 3, 0,  0, 7, 0,  0, 0, 0],
-                 [6, 0, 0,  1, 9, 5,  0, 0, 0],
-                 [0, 9, 8,  0, 0, 0,  0, 6, 0],
+           [6, 0, 0,  1, 9, 5,  0, 0, 0],
+           [0, 9, 8,  0, 0, 0,  0, 6, 0],
 
-                 [8, 0, 0,  0, 6, 0,  0, 0, 3],
-                 [4, 0, 0,  8, 0, 3,  0, 0, 1],
-                 [7, 0, 0,  0, 2, 0,  0, 0, 6],
+           [8, 0, 0,  0, 6, 0,  0, 0, 3],
+           [4, 0, 0,  8, 0, 3,  0, 0, 1],
+           [7, 0, 0,  0, 2, 0,  0, 0, 6],
 
-                 [0, 6, 0,  0, 0, 0,  2, 8, 0],
-                 [0, 0, 0,  4, 1, 9,  0, 0, 5],
-                 [0, 0, 0,  0, 8, 0,  0, 7, 0]]
+           [0, 6, 0,  0, 0, 0,  2, 8, 0],
+           [0, 0, 0,  4, 1, 9,  0, 0, 5],
+           [0, 0, 0,  0, 8, 0,  0, 7, 0]]
 
+--Resuelve encuentra el primer tablero completado
 resuelve :: Tablero -> Maybe Tablero
-resuelve = encontrado . solucións
+resuelve = encontrado . soluciones
 
-solucións :: Tablero -> [Tablero]
-solucións b = solucións' (emptyPosicions b) b
+--Busca las posibles soluciones de un tablero
+soluciones :: Tablero -> [Tablero]
+soluciones b = soluciones' (posicionEn0 b) b
   where
-    solucións' :: [Posicion] -> Tablero -> [Tablero]
-    solucións' []     b = [b]
-    solucións' (x:xs) b = concatMap (solucións' xs) candidateTableros
+    soluciones' :: [Posicion] -> Tablero -> [Tablero]
+    soluciones' []     b = [b]
+    soluciones' (x:xs) b = concatMap (soluciones' xs) candidateTableros
       where
-        candidateMarcados  = [m | m <- [1..9], isPossibleMarcado m x b]
-        candidateTableros = map (\m -> copyWithMarcado m x b) candidateMarcados
+        candidateNumeros  = [m | m <- [1..9], posibleNumero m x b]
+        candidateTableros = map (\m -> copiaDeNumero m x b) candidateNumeros
 
-emptyPosicions :: Tablero -> [Posicion]
-emptyPosicions b = [(row, col) | row <- [0..8], col <- [0..8], b ! (row, col) == 0]
+--determina si en una posicion no hay valor asignado 
+posicionEn0 :: Tablero -> [Posicion]
+posicionEn0 b = [(fila, columna) | fila <- [0..8], columna <- [0..8], b ! (fila, columna) == 0]
 
-isPossibleMarcado :: Marcado -> Posicion -> Tablero -> Bool
-isPossibleMarcado m (row, col) b = notInRow && notInColumn && notInBox
+--Obtinene un posible numero si es que no hay cierto numero en la fila, columnaumna o cuadrante
+posibleNumero :: Numero -> Posicion -> Tablero -> Bool
+posibleNumero m (fila, columna) b = noEnCuadrante && noEnFila && noEnColuna
   where
-    notInRow    = notElem m $ b `marcasEnFila` row
-    notInColumn = notElem m $ b `marcasEnColumna` col
-    notInBox    = notElem m $ b `marcasEnCuadrado` (row, col)
+    noEnCuadrante = notElem m $ b `marcasEnCuadrante` (fila, columna)
+    noEnFila = notElem m $ b `marcasEnFila` fila
+    noEnColuna = notElem m $ b `marcasEnColumna` columna
 
-copyWithMarcado :: Marcado -> Posicion -> Tablero -> Tablero
-copyWithMarcado mark (row, col) b = b // [((row, col), mark)]
+--Copia el numero
+copiaDeNumero :: Numero -> Posicion -> Tablero -> Tablero
+copiaDeNumero mark (fila, columna) b = b // [((fila, columna), mark)]
 
-marcasEnFila :: Tablero -> Int -> [Marcado]
-b `marcasEnFila` row = [b ! loc | loc <- range((row, 0), (row, 8))]
-
-marcasEnColumna ::  Tablero -> Int -> [Marcado]
-b `marcasEnColumna` col = [b ! loc | loc <- range((0, col), (8, col))]
-
-marcasEnCuadrado :: Tablero -> Posicion -> [Marcado]
-b `marcasEnCuadrado` (row, col) = [b ! loc | loc <- locations]
+--busca concurrencias en el cuadrante
+marcasEnCuadrante :: Tablero -> Posicion -> [Numero]
+b `marcasEnCuadrante` (fila, columna) = [b ! loc | loc <- locations]
   where
-    row' = (row `div` 3) * 3
-    col' = (col `div` 3) * 3
-    locations = range((row', col'), (row' + 2, col' + 2))
+    fila' = (fila `div` 3) * 3
+    columna' = (columna `div` 3) * 3
+    locations = range((fila', columna'), (fila' + 2, columna' + 2))
 
-verTablero :: [[Marcado]] -> [(Posicion, Marcado)]
-verTablero = concatMap verFila . zip [0..8]
+--Busca concurrencias en la fila actual
+marcasEnFila :: Tablero -> Int -> [Numero]
+b `marcasEnFila` fila = [b ! loc | loc <- range((fila, 0), (fila, 8))]
+
+--Busca concurrencias en la columna actual
+marcasEnColumna ::  Tablero -> Int -> [Numero]
+b `marcasEnColumna` columna = [b ! loc | loc <- range((0, columna), (8, columna))]
+
+--Crea el tablero de sudoku a partir de la entrada como el ejemplo
+creaTablero :: [[Numero]] -> [(Posicion, Numero)]
+creaTablero = concatMap creaFila . zip [0..8]
   where
-    verFila :: (Int, [Marcado]) -> [((Int, Int), Marcado)]
-    verFila (row, marks) = verCol row $ zip [0..8] marks
+    creaFila :: (Int, [Numero]) -> [((Int, Int), Numero)]
+    creaFila (fila, marks) = creaColumna fila $ zip [0..8] marks
 
-    verCol :: Int -> [(Int, Marcado)] -> [((Int, Int), Marcado)]
-    verCol row cols = map (\(col, m) -> ((row, col), m)) cols
+    creaColumna :: Int -> [(Int, Numero)] -> [((Int, Int), Numero)]
+    creaColumna fila columnas = map (\(columna, m) -> ((fila, columna), m)) columnas
 
+--Determina si se ha encontrado un tablero solucion
 encontrado :: [a] -> Maybe a
 encontrado []     = Nothing
 encontrado (x:xs) = Just x
 
+--Se imprime el arreglo solucion
 printTablero :: Maybe Tablero -> IO ()
 printTablero Nothing  = putStrLn "Sin solucion"
-printTablero (Just b) = mapM_ putStrLn [show $ b `marcasEnFila` row | row <- [0..8]]
+printTablero (Just b) = mapM_ putStrLn [show $ b `marcasEnFila` fila | fila <- [0..8]]
+
+main = do
+    let solucion = resuelve tableroSudoku
+    printTablero solucion
